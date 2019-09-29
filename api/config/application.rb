6838ea262afdfa0af531822e22a2f3f0c322eb17
Igor Fernandes <ifc@cin.ufpe.br>
2019-09-28 22:16:03 -0300

    [API] Adicionado domínio ao Rails
    O Rails agora entende o 'api.applada.com.br' como um host válido

--- api/config/application.rb
+++ api/config/application.rb
@@ -33,5 +33,8 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Domain Host
+    config.hosts << "api.applada.com.br"
   end
 end
