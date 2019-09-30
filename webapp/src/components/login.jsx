80a70b2c1d8be9b39a02c1dc86090368d9e68ba6
lucas <lucaspradocb@gmail.com>
2019-09-30 20:23:57 -0300

    login form component created

--- webapp/src/components/login.jsx
+++ webapp/src/components/login.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import './../style/login.css'
 
 class Login extends React.Component {
+    state = {
+
+    }
+
     render() {
         return (
             <div className='login-container'>
-                componente de login 
+                <form className='login-form'>
+                    <label>E-MAIL:</label>
+                    <input type='text' className='login-form-inputs'/>
+                    <label>SENHA:</label>
+                    <input type='password' className='login-form-inputs'/>
+                    <input type='submit' value='ENTRAR' id='submit-login-form'/>
+                </form>
             </div>
         )
     }
