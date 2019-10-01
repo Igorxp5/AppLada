1856efa711be8455cdfc7ce8f2ae6f22bdd9dd1d
Marcos Galvão <matg@cin.ufpe.br>
2019-10-01 04:18:23 +0000

    cucumber

--- api/features/step_definitions/login.rb
+++ api/features/step_definitions/login.rb
@@ -1,7 +1,26 @@
+Before do
+  #@user_teste = User.create
+  #@user_teste.login = "username_teste",
+  #@user_teste.name = "Testador do time",
+  #@user_teste.email = "username_teste@teste.com",
+  #@user_teste.password = "senha_teste",
+  #@user_teste.birthday = "30/01/2000",
+  #@user_teste.gender = "M"
+  
+  @user_teste = User.create(login: "username_teste", email: "username_teste@teste.com",
+                     password: "senha_teste", name: "Testador do time",
+                     birthday: "30/01/2000", gender: "M")
+  @user_teste.save
+end
+
+After do
+  print User.find_by_login("username_teste")
+  print "AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"
+end
+
 Dado("que uma chamada POST seja feita no endpoint de login") do
   pending
 end
-
 
 Quando("o campo {string} estiver preenchido com {string}") do |string, string2|
   pending # Write code here that turns the phrase above into concrete actions
