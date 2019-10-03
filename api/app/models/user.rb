045e9585021e8241e523ba7562a4232fc9de41b2
Marcos Galvão <matg@cin.ufpe.br>
2019-10-03 04:35:01 +0000

    [Api] Corrigido validação de dados no model user A validação de data de nascimento e nome foram corrigidas.

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
 	validates :email, presence: true, uniqueness: true
 	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 	validates :login, presence: true, uniqueness: true
-	validates :name, presence: true, format: {with: /\A[a-z]+\Z/i, message: 'can have only letters'}
+	validates :name, presence: true, format: {with: /\A[\p{L} ]+\z/, message: 'can have only letters'}
 	validates :birthday, presence: true
 	validates :gender, presence: true
 	validates :password,
 	        length: { minimum: 8, maximum: 16 },
 	        if: -> { new_record? || !password.nil? }
 
-	validates_each :birthday do |record, attr, value|
-      record.errors.add(attr, "can't be in the past") if value >= Time.now.to_date
+		validates_each :birthday do |record, attr, value|
+      record.errors.add(attr, "can't be in the future") if not value.nil? and value >= Time.now.to_date
     end
     validates_each :gender do |record, attr, value|
       record.errors.add(attr, "must be 'M', 'F', or 'O'") unless ['M', 'F', 'O'].include? value
