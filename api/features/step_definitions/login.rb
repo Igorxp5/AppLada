f99b27a3c221cc992623425aed938cb356a7a3d6
Igor Fernandes <ifc@cin.ufpe.br>
2019-10-03 03:51:47 -0300

    [Deploy] Subdomínio para teste
    Para acessar os testes deve ser usado o subdomínio: test.applada.com.br

--- api/features/step_definitions/login.rb
+++ api/features/step_definitions/login.rb
@@ -3,7 +3,7 @@ Before do
     user_teste = User.create!(login: "username_teste", email: "username_teste@teste.com",
                         password: "senha_teste", name: "TestadorDoTime",
                         birthday: "30/01/2000", gender: "M")
-    header 'Host', 'api.applada.com.br'
+    header 'Host', 'test.applada.com.br'
 end
 
 After do
