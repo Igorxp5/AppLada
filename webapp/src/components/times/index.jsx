689387423334c6fa117d7c989fc0fc9ccd512f2a
lucas <lucaspradocb@gmail.com>
2019-11-19 21:25:45 -0300

    Consertar links para mudar pagina

---
+++ webapp/src/components/times/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './../../style/times/times-index.css'
+
+class Times extends React.Component {
+    render() {
+        return(
+            <div id='times-container'>TIMES!!!!!!!!</div>
+        )
+    }
+}
+
+export default Times
